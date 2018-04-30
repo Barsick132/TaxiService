2f806c3f4ce3dc372876262d00ed10385cace185
Barsick132 <antony.nor@gmail.com>
2018-04-30 22:00:43 +0300

    [~]Обновил Gradle до версии 4.5.1. Незначительно отредактировал файлы build.gradle по аналогии с примером проекта с MapKit от Яндекса. И самое главное решил ошибку, которая была связана с подключение MapKit 3.0, путем отключения D8 в файле gradle.properties строчкой: android.enableD8=false. [+]Возникла проблема с отображением Яндекс Карты. Кажется это связано с активацией ключа. Написал в техподдержку для решения проблемы.

--- app/src/main/java/com/lstu/kovalchuk/taxiservice/Global.java
+++ app/src/main/java/com/lstu/kovalchuk/taxiservice/Global.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 
-//import com.yandex.mapkit.Animation;
-//import com.yandex.mapkit.MapKitFactory;
-//import com.yandex.mapkit.geometry.Point;
-//import com.yandex.mapkit.map.CameraPosition;
-//import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
 
 /*
 import ru.yandex.yandexmapkit.MapController;
@@ -20,21 +20,21 @@ import ru.yandex.yandexmapkit.utils.GeoPoint;
 
 public class Global extends Activity {
 
-    //private MapView mapview;
+    private MapView mapview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //MapKitFactory.setApiKey("ce22acd0-3f1a-4e4b-bb04-293ac4af654a");
-        //MapKitFactory.initialize(this);
+        MapKitFactory.setApiKey("ce22acd0-3f1a-4e4b-bb04-293ac4af654a");
+        MapKitFactory.initialize(this);
 
         setContentView(R.layout.activity_global);
-        /*mapview = (MapView)findViewById(R.id.globalMap);
+        mapview = (MapView)findViewById(R.id.globalMap);
         mapview.getMap().move(
                 new CameraPosition(new Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
                 new Animation(Animation.Type.SMOOTH, 0),
-                null);*/
+                null);
 
         /*
         final MapView mMapView = (MapView) findViewById(R.id.globalMap1);
@@ -54,15 +54,15 @@ public class Global extends Activity {
     @Override
     protected void onStop() {
         super.onStop();
-        //mapview.onStop();
-        //MapKitFactory.getInstance().onStop();
+        mapview.onStop();
+        MapKitFactory.getInstance().onStop();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        //mapview.onStart();
-        //MapKitFactory.getInstance().onStart();
+        mapview.onStart();
+        MapKitFactory.getInstance().onStart();
     }
 
     private void initToolbar() {
